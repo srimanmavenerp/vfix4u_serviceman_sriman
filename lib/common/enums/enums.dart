776eafe902e6c5c40bb70def4b4f4da243abbcdd
NarayanaErp <narayana@mavenerp.in>
2025-10-31 10:26:49 +0530

    third  commit for Vfix4u Serviceman project

--- lib/common/enums/enums.dart
+++
@@ -1,8 +0,0 @@
-enum HtmlType { termsAndCondition, aboutUs, privacyPolicy, refundPolicy, returnPolicy, cancellationPolicy}
-enum SendOtpType {forgetPassword, firebase, verification}
-enum NoDataType { notification, booking, others}
-enum BooingListStatus{accepted,ongoing}
-enum BookingDetailsTabControllerState {bookingDetails,status}
-enum EarningType{monthly, yearly}
-enum EditProfileTabControllerState {generalInfo,accountIno}
-enum ToasterMessageType {success, error, info}
--- lib/common/models/language_model.dart
+++
@@ -1,7 +0,0 @@
-class LanguageModel {
-  String? imageUrl;
-  String? languageName;
-  String? languageCode;
-  String? countryCode;
-  LanguageModel({this.imageUrl, this.languageName, this.countryCode, this.languageCode});
-}
